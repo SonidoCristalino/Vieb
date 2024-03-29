f8d7cc523801a49b67be7da58152ca3004b4c4ed
Emiliano <gradoinfinitesimal@gmail.com>
2024-03-29 15:16:43 -0300

    Initial Commit

---
+++ userscript/linkedin.com.js
@@ -0,0 +1,10 @@
+
+// Delete all the non-using element 
+document.querySelector("#global-nav > div > nav > ul > li:nth-child(1)").remove();
+document.querySelector("#global-nav > div > nav > ul > li:nth-child(4)").remove();
+document.querySelector("#main > div > div:nth-child(1) > div:nth-child(1)").remove();
+document.querySelector("#main > div > div:nth-child(1) > div").remove();
+document.querySelector("#messaging > div > div > aside").remove();
+document.querySelector("#global-nav-search > div").remove();
+// document.querySelector("").remove();
+
---
+++ userscript/whatsapp.com.js
@@ -0,0 +1,8 @@
+// We remove the search bar
+// document.querySelector("#side > div._3gYev").remove();
+document.querySelector("#side > div._ak9t").remove();
+
+// We remove all the chat header 
+// document.querySelector("#app > div > div.two._1jJ70 > div._2Ts6i._3RGKj > header").remove();
+document.querySelector("#app > div > div.two._aigs > div._aigv._aigw > header").remove();
+
