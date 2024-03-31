bf84a2ad31067378ea3ae9f680a79bcf0415cbab
Emiliano <gradoinfinitesimal@gmail.com>
2024-03-31 14:58:06 -0300

    Add new userscript for linkedin chat

---
+++ userscript/gm/linkedin.com.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        f*ck linkedin
+// @namespace   Violentmonkey Scripts
+// @match       https://www.linkedin.com/feed/
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 4/27/2022, 3:20:07 PM
+// @license     MIT
+// ==/UserScript==
+ 
+(function() {
+    'use strict';    
+    document.getElementById('main').remove();    
+})();
