38ccb4fd9ad7f954b0da586991b23825d9d5be93
Emiliano <gradoinfinitesimal@gmail.com>
2024-03-31 21:08:11 -0300

    GITHUB-1: Clear all unnecessary div from Linkedin messaging page

--- userscript/gm/linkedin.com.chat.js
+++ userscript/gm/linkedin.com.chat.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        Expand small chat on Linkedin
+// @name        Expand functionality chat on Linkedin
 // @namespace   Violentmonkey Scripts
 // @match       https://www.linkedin.com/messaging/*
 // @grant       none
-// @version     0.1
-// @author      Siarhei Siniak
-// @license Unlicense
-// @description 11/4/2023, 8:44:59 PM
+// @version     0.5
+// @author      Siarhei Siniak & others. 
+// @license     Unlicense
+// @description Try to improve LinkedIn chat removing elements that prevent good experience.
  
 // @inject-into document
 // @noframes
@@ -14,10 +14,26 @@
  
 setTimeout(() => {
   
-  let $$ = (s) => [document.querySelector(s)];
-  $$('#messaging .scaffold-layout__content')[0].setAttribute('style', 'display: unset');
-  $$('#main')[0].setAttribute('style', 'height: 100vh');
-  $$('.scaffold-layout__aside')[0].setAttribute('style', 'display: none');
-  $$('.premium-upsell-link')[0].setAttribute('style', 'display: none');
+    let $$ = (s) => [document.querySelector(s)];
+    $$('#messaging .scaffold-layout__content')[0].setAttribute('style', 'display: unset');
+    $$('#main')[0].setAttribute('style', 'height: 100vh');
+    $$('.scaffold-layout__aside')[0].setAttribute('style', 'display: none');
+    
+    // Remove inecessary primary search bar
+    $$('#global-nav-search')[0].setAttribute('style', 'display: none');
+
+    // Remove inecessary secondary search bar
+    $$('.msg-search-form')[0].setAttribute('style', 'display: none');
+
+    // Remove inecessary Premium link
+    $$('.premium-upsell-link')[0].setAttribute('style', 'display: none');
+
+    // Remove messages filter
+    document.querySelectorAll('.msg-conversations-container__title-row').forEach(Element => {
+        Element.remove();});
+
+    // Remove all the config setting in each conversation card
+    document.querySelectorAll(".msg-conversation-card__inbox-shortcuts").forEach(Element => {
+        Element.remove();})
  
 }, 3000)
