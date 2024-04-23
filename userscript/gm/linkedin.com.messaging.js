18e46ff587e22a6a456d84dc165496b8529e74f5
Emiliano <gradoinfinitesimal@gmail.com>
2024-04-23 12:23:07 -0300

    Try to resolve message fatal behavior

--- userscript/gm/linkedin.com.messaging.js
+++ userscript/gm/linkedin.com.messaging.js
@@ -15,21 +15,23 @@
 setTimeout(() => {
   
     let $$ = (s) => [document.querySelector(s)];
-    $$('#messaging .scaffold-layout__content')[0].setAttribute('style', 'display: unset');
-    $$('#main')[0].setAttribute('style', 'height: 100vh');
-    $$('.scaffold-layout__aside')[0].setAttribute('style', 'display: none');
+    $$("#messaging .scaffold-layout__content")[0].setAttribute("style", "display: unset");
+    $$(".scaffold-layout__aside")[0].setAttribute("style", "display: none");
+    // $$("#main")[0].setAttribute("style", "width: 120vh");
+    $$(".scaffold-layout__main")[0].setAttribute("style", "width: 120vh");
     
+
     // Remove inecessary primary search bar
-    $$('#global-nav-search')[0].setAttribute('style', 'display: none');
+    $$("#global-nav-search")[0].setAttribute("style", "display: none");
 
     // Remove inecessary secondary search bar
-    $$('.msg-search-form')[0].setAttribute('style', 'display: none');
+    $$(".msg-search-form")[0].setAttribute("style", "display: none");
 
     // Remove inecessary Premium link
-    $$('.premium-upsell-link')[0].setAttribute('style', 'display: none');
+    $$(".premium-upsell-link")[0].setAttribute("style", "display: none");
 
     // Remove messages filter
-    document.querySelectorAll('.msg-conversations-container__title-row').forEach(Element => {
+    document.querySelectorAll(".msg-conversations-container__title-row").forEach(Element => {
         Element.remove();});
 
     // Remove all the config setting in each conversation card
