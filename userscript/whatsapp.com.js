b7db7e5859b8dfc8a6f9f9b0c907e6dc5eabe324
Emiliano <gradoinfinitesimal@gmail.com>
2024-05-18 13:05:51 -0300

    Remove whatsapp web header from main screen

--- userscript/whatsapp.com.js
+++ userscript/whatsapp.com.js
@@ -6,3 +6,6 @@ document.querySelector("#app > div > div.two._aigs > div._aigv._aigw > header").
 
 // Remove popup helper chat
 document.querySelector("#wa-popovers-bucket").remove();
+
+// Remove chat filter 
+document.querySelector("#side > div.x1ky8ojb.x78zum5.x1q0g3np.x1a02dak.x2lah0s.x3pnbk8.xfex06f.xeuugli.x2lwn1j.x1nn3v0j.x1ykpatu.x1swvt13.x1pi30zi").remove();
