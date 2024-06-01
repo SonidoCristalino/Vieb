371d624c689de15a5e9474449bb52ad7f79b7846
Emiliano <gradoinfinitesimal@gmail.com>
2024-06-01 13:59:36 -0300

    TRELLO-117: Improve view in new job recommended page

---
+++ userscript/gm/simpyApply.linkedin.com.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        F*ck Banner LinkedIn
+// @namespace   Violentmonkey Scripts
+// @match       https://www.linkedin.com/jobs/search/?alertAction=viewjobs*
+// @grant       none
+// @version     1.0
+// @author      Me
+// @license     MIT
+// @description In job search section, remove some unecessary objects
+ 
+// @inject-into document
+// @noframes
+// ==/UserScript==
+
+setTimeout(() => {
+  
+    let $$ = (s) => [document.querySelector(s)];
+    
+    // Remove unecessary Premium link
+    $$(".premium-upsell-link")[0].setAttribute("style", "display: none");
+
+    // Remove unecessary primary search bar
+    $$("#global-nav-search")[0].setAttribute("style", "display: none");
+
+    // Remove unnecessary chat list
+    $$(".msg-overlay-list-bubble")[0].setAttribute("style", "display: none");
+    
+    // Remove unnecessary toolbar filters
+    $$(".scaffold-layout-toolbar")[0].setAttribute("style", "display: none");
+
+}, 3000)
