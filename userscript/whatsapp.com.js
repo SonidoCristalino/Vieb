966acba2af99e0887637aedbe934d69695b2fd98
Emiliano <gradoinfinitesimal@gmail.com>
2024-06-23 11:59:27 -0300

    TRELLO-135: unset visibility to Archive chat

--- userscript/whatsapp.com.js
+++ userscript/whatsapp.com.js
@@ -9,3 +9,31 @@ document.querySelector("#wa-popovers-bucket").remove();
 
 // Remove chat filter 
 document.querySelector("#side > div.x1ky8ojb.x78zum5.x1q0g3np.x1a02dak.x2lah0s.x3pnbk8.xfex06f.xeuugli.x2lwn1j.x1nn3v0j.x1ykpatu.x1swvt13.x1pi30zi").remove();
+
+// At the beginning unset Archive botton visibility 
+document.querySelector("#pane-side > button").setAttribute("style", "display: none");
+
+// Use a function to set or unset Archive visibility
+(function() {
+    'use strict';
+
+    var elemento = document.querySelector("#pane-side > button");
+    var visible = false;
+
+    function alternarVisibilidad() {
+        if (visible) {
+            elemento.style.display = "none";
+        } else {
+            elemento.style.display = ""; 
+        }
+        visible = !visible;
+    }
+
+    // Add shortkey to set Archive visibility
+    document.addEventListener('keydown', function(event) {
+        if (event.altKey && event.shiftKey && event.key === 'M') {
+            alternarVisibilidad();
+        }
+    });
+
+})();
