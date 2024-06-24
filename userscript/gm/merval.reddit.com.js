1de0801a1ade781df42f17d64fd836b8c2875a2f
Emiliano <gradoinfinitesimal@gmail.com>
2024-06-23 23:33:03 -0300

    TRELLO-135: improve Reddit page just like Whatsapp behaviour

--- userscript/gm/merval.reddit.com.js
+++ userscript/gm/merval.reddit.com.js
@@ -5,14 +5,42 @@
 // @description  Remove some annoying things
 // @author       Me
 // @license      MIT
-// @match        https://*.reddit.com/r/merval/*
+// @match        https://*.reddit.com/*
 // @icon         https://reddit.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-// Remove right panel
-document.querySelector("#right-sidebar-container").remove();
+// Remove right panel at the beginning
+document.querySelector("#right-sidebar-container").setAttribute("style", "display: none");
 
-// Increase max width in post
-document.querySelector("#main-content")
-    .setAttribute("style", "max-width: none");
+// Increase max width in every post
+document.querySelector("#main-content").setAttribute("style", "max-width: none");
+
+// Use a function to set or unset sidebar visibility
+(function() {
+    'use strict';
+
+    var sidebar        = document.querySelector("#right-sidebar-container");
+    var sticky_sidebar = document.querySelector('div[data-testid="search-results-sticky-sidebar"]');
+    var visible        = false;
+
+    function alternateVisibility() {
+        
+        if (sidebar) {
+            sidebar.style.display = visible ? "none" : "";
+        }
+
+        if (sticky_sidebar) {
+            sticky_sidebar.style.display = visible ? "none" : "";
+        }
+        
+        visible = !visible;
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.altKey && event.shiftKey && event.key === 'M') {
+            alternateVisibility();
+        }
+    });
+
+})();
