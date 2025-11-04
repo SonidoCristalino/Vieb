7f3cf8b061c273fbe9517efbe5b42eb82d14479b
Emiliano <gradoinfinitesimal@gmail.com>
2025-11-04 15:32:08 -0300

    Upgrade whatsapp userscript

--- userscript/whatsapp.com.js
+++ userscript/whatsapp.com.js
@@ -27,19 +27,19 @@ document.querySelector("#side > div._ak9t").remove();
 // NOTE: If you want the settings to appear (such as to mute the sounds of
 // tabs) you must comment the line below.
 document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/header',
-    document, null, XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null).singleNodeValue.style.display = 'none';
+    document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+    .setAttribute("style", "display: none");
+    // .style.display = 'none';
 
 // Chats header 
-document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[3]/div/div[1]',
+document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[4]/div/div[1]',
     document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     .setAttribute("style", "display: none");
 
 // Whatsapp header 
-document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[3]/header',
+document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[4]/header/header',
     document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     .setAttribute("style", "display: none");
-
 
 // ###############################################################################
 // #                             Elements Modifications                          #
@@ -47,7 +47,7 @@ document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[3]/header
 
 // The margin of the archived chats is removed so that it does not overlap with
 // the chats behind. 
-document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[2]/div[1]',
+document.evaluate('/html/body/div[1]/div/div/div[1]/div/div[3]/div/div[3]/div[1]',
     document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
     .singleNodeValue.setAttribute("style", "margin-left: 0px");
 
